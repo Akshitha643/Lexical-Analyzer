c4fe4120e3beedf8af9def9b9c4d6b1946418394
G Akshitha Reddy <gundalaakshithareddy@gmail.com>
2026-06-18 10:48:33 +0530

    Update lexer.c

--- lexer.c
+++ lexer.c
@@ -3,14 +3,19 @@
 #include<ctype.h>
 #include "lexer.h"
 
+/* Array containing data type keywords */
 char *datakeywords[]={"int","float","char","double","short",
-    "long","signed","unsigned","void"};   //datatypes
-char *nonDatakeywords[]={"goto","return","continue","break","if",
-    "for","else","while","do","switch","case","default","sizeof"}; //reserved (not datatypes)
+    "long","signed","unsigned","void"};
 
+/* Array containing non-data type keywords */
+char *nonDatakeywords[]={"goto","return","continue","break","if",
+    "for","else","while","do","switch","case","default","sizeof"}; 
+
+/* Check whether the given token is a data type keyword */
 int isDataKeyword(char *str)
 {
-    int n=sizeof(datakeywords)/sizeof(datakeywords[0]);  //to calculate no.of elements in array
+    //to calculate no.of elements in array
+    int n=sizeof(datakeywords)/sizeof(datakeywords[0]);  
     for(int i=0;i<n;i++)
     {
         if(strcmp(str,datakeywords[i])==0)
@@ -20,6 +25,7 @@ int isDataKeyword(char *str)
     }
     return 0;
 }
+/* Check whether the given token is a non-data type keyword */
 int isnonDataKeyword(char *str)
 {
     int n=sizeof(nonDatakeywords)/sizeof(nonDatakeywords[0]);
@@ -32,16 +38,19 @@ int isnonDataKeyword(char *str)
     }
     return 0;
 }
+/* Check whether the character is an operator */
 int isOperator(char ch)
 {
     return(ch=='+' || ch=='-' || ch=='*' || ch=='/' || ch=='%' || ch=='=' ||
            ch=='<' || ch=='>');
 }
+/* Check whether the character is an special character */
 int isSpclChar(char ch)
 {
     return(ch=='(' || ch==')' || ch=='{' || ch=='}' || ch=='[' || ch==']' ||
            ch==';' || ch==',');
 }
+/* Check whether the token is an integer literal */
 int isInteger(char *str)
 {
     int i;
@@ -65,8 +74,12 @@ int isFloat(char *str)
         else if(!isdigit(str[i]))
             return 0;
     }
+     /* Valid float should contain exactly one decimal point */
     return (dot==1);
 }
+
+
+
 
 
 
